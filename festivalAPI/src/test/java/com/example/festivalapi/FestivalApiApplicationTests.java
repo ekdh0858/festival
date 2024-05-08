52b34c7689a9e702379e188ce3e0b24d371e00bd
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2024-05-08 11:59:58 +0900

    initial commit
    API 만들기 위한 시작 커밋.
    gitignore 추가
    lombok, web, JPA 의존 추가

---
+++ festivalAPI/src/test/java/com/example/festivalapi/FestivalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.festivalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
